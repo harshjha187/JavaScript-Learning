c6437386fe5a05c3cee74e20c48c886ee13dfba5
harshjha187 <2422269.iot.cec@cgc.edu.in>
2026-08-02 19:33:46 +0000

    Learned let, const and var

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const Name= "Harsh"
+let Age=20
+const College="CGC Landran"
+const Branch="CSE IOT"
+const Skill="JS Beginner"
+const majorProject="Bluid Innovator OS"
+const Cgpa
+
+// prefer not to use var 
+// due to issue in block scope and functional scope
+
+console.table([Name,Age,College,Branch,Skill,majorProject,Cgpa])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,3 @@
+"use strict"; // treat al js code as newer version
+
+alert(3+3) //not work here beacuse we're not using browser using nodejs
