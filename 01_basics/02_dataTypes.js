cc870c46d9f01b3f20448fd146bb8b408e99d0f2
harshjha187 <2422269.iot.cec@cgc.edu.in>
2026-08-02 19:46:43 +0000

    Completed datatypes Class

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -1,3 +1,24 @@
 "use strict"; // treat al js code as newer version
 
 alert(3+3) //not work here beacuse we're not using browser using nodejs
+
+
+let name = "Harsh"
+let age = 20
+let isLoggedIn = false
+let state;
+
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
